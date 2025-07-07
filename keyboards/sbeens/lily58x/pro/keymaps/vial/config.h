a12bfee391c5025263819558a0f995b434c2d133
Ragnar Petursson <74888286+kvikindi@users.noreply.github.com>
2025-07-07 10:08:22 +0100

    messy init commit

---
+++ keyboards/sbeens/lily58x/pro/keymaps/vial/config.h
@@ -0,0 +1,36 @@
+/* Copyright 2020 Josef Adamcik
+ * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
+ * Modification for Vial support by Drew Petersen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// clang-format off
+
+#pragma once
+
+// #define EE_HANDS
+// #define USE_SERIAL_PD2
+
+// Vial Support
+#define VIAL_KEYBOARD_UID {0x8E, 0xD7, 0xBF, 0xCB, 0x81, 0x07, 0xC3, 0x36}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_COMBO_ENTRIES 64
+#define VIAL_KEY_OVERRIDE_ENTRIES 64
+#define VIAL_TAP_DANCE_ENTRIES 64
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 20480
+
+// #define VIALRGB_NO_DIRECT
